fd60f0b49ed934293bebd2890abce854b98416c8
Etienne LARROUMETS <e.larroumets@hotmail.fr>
2022-05-30 20:28:22 +0200

    Redesign des formulaires de connexion et enregistrement

--- resources/views/web/backend/auth/login.blade.php
+++ resources/views/web/backend/auth/login.blade.php
@@ -4,19 +4,24 @@
 
 @section('main')
     @include('components.message')
-    <div class="col-md-4 sm-12 mx-auto">
-        {!! Form::open(['route' => 'login']) !!}
-        <!-- Username-->
-        <div class="form-group row">
-            {!! Form::label('email', 'Email :', ['class' => 'col-sm-4 col-form-label']) !!}
-            {!! Form::text('email', null, ['class' => 'col-sm-8 form-control required']) !!}
-        </div>
-        <!-- Password -->
-        <div class="form-group row">
-            {!! Form::label('password', 'Mot de passe :', ['class' => 'col-sm-4 col-form-label']) !!}
-            {!! Form::password('password', null, ['class' => 'col-sm-8 form-control required']) !!}
-        </div>
-        {!! Form::submit('Se connecter') !!}
-        {!! Form::close() !!}
+    <h4 class="text-center">Connexion à l'interface d'administration</h4>
+    <div class="row">
+      <div class="col-md-4 col-sm-12 col-md-offset-4">
+          {!! Form::open(['route' => 'login']) !!}
+          <!-- Username-->
+          <div class="form-group row">
+              {!! Form::label('email', 'Email :', ['class' => 'col-form-label']) !!}
+              {!! Form::text('email', null, ['class' => 'form-control' , 'required' => 'true']) !!}
+          </div>
+          <!-- Password -->
+          <div class="form-group row">
+              {!! Form::label('password', 'Mot de passe :', ['class' => 'col-form-label']) !!}
+              {!! Form::password('password', ['class' => 'form-control', 'required' => 'true']) !!}
+          </div>
+          <div class="form-group row">
+            {!! Form::submit('Se connecter', ['class' => 'btn btn-block btn-primary']) !!}
+          </div>
+          {!! Form::close() !!}
+      </div>
     </div>
 @endsection
--- resources/views/web/backend/auth/register.blade.php
+++ resources/views/web/backend/auth/register.blade.php
@@ -4,29 +4,32 @@
 
 @section('main')
     @include('components.message')
-    <div class="col-md-4 sm-12 mx-auto">
+    <h4 class="text-center">Enregistrement d'un administrateur</h4>
+    <div class="col-md-4 col-sm-12 col-md-offset-4">
         {!! Form::open(['route' => 'register']) !!}
         <!-- Name-->
         <div class="form-group row">
-            {!! Form::label('name', 'Nom :', ['class' => 'col-sm-4 col-form-label']) !!}
-            {!! Form::text('name', null, ['class' => 'col-sm-8 form-control required']) !!}
+            {!! Form::label('name', 'Nom :', ['class' => 'col-form-label']) !!}
+            {!! Form::text('name', null, ['class' => 'form-control' , 'required' => 'true']) !!}
         </div>
         <!-- Username/Email-->
         <div class="form-group row">
-            {!! Form::label('email', 'Email :', ['class' => 'col-sm-4 col-form-label']) !!}
-            {!! Form::text('email', null, ['class' => 'col-sm-8 form-control required']) !!}
+            {!! Form::label('email', 'Email :', ['class' => 'col-form-label']) !!}
+            {!! Form::text('email', null, ['class' => 'form-control' , 'required' => 'true']) !!}
         </div>
         <!-- Password -->
         <div class="form-group row">
-            {!! Form::label('password', 'Mot de passe :', ['class' => 'col-sm-4 col-form-label']) !!}
-            {!! Form::password('password', null, ['class' => 'col-sm-8 form-control required']) !!}
+            {!! Form::label('password', 'Mot de passe :', ['class' => 'col-form-label']) !!}
+            {!! Form::password('password', ['class' => 'form-control' , 'required' => 'true']) !!}
         </div>
         <!-- Password confirmation -->
         <div class="form-group row">
-            {!! Form::label('password_confirmation', 'Confirmation mot de passe :', ['class' => 'col-sm-4 col-form-label']) !!}
-            {!! Form::password('password_confirmation', null, ['class' => 'col-sm-8 form-control required']) !!}
+            {!! Form::label('password_confirmation', 'Confirmation mot de passe :', ['class' => 'col-form-label']) !!}
+            {!! Form::password('password_confirmation', ['class' => 'form-control' , 'required' => 'true']) !!}
         </div>
-        {!! Form::submit('Se connecter') !!}
+        <div class="form-group row">
+          {!! Form::submit('Enregistrer', ['class' => 'btn btn-block btn-primary']) !!}
+        </div>
         {!! Form::close() !!}
     </div>
 
